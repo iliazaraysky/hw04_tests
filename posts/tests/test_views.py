792981bc736d5e67d520575958cb166d0ce26ed7
Ilia Zaraysky <nextontext@yandex.ru>
2021-03-23 17:24:18 +0300

    Add image context tests

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, Client
 from django.urls import reverse
 from django import forms
-import random
+import random, tempfile, os, pathlib
 
 from posts.models import Group, Post
 
@@ -27,11 +28,27 @@ class ProjectViewsTests(TestCase):
             email='testuser@yatube.ru'
         )
 
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
+
         for i in range(1, 14):
             cls.post = Post.objects.create(
                 group=ProjectViewsTests.group,
                 text="Какой-то там текст",
                 author=User.objects.get(username='authorForPosts'),
+                image=uploaded
             )
 
     def setUp(self):
@@ -60,6 +77,7 @@ class ProjectViewsTests(TestCase):
         form_fields = {
             'group': forms.fields.ChoiceField,
             'text': forms.fields.CharField,
+            'image': forms.fields.ImageField,
         }
 
         for value, expected in form_fields.items():
@@ -75,8 +93,10 @@ class ProjectViewsTests(TestCase):
         author = response.context['author']
         post_text_0 = post.text
         post_author_0 = author.first_name
+        post_image_0 = post.image
         self.assertEqual(post_author_0, 'Тестов')
         self.assertEqual(post_text_0, 'Какой-то там текст')
+        self.assertEqual(post_image_0, self.post.image.name)
 
     def test_context_in_post_edit(self):
         """
@@ -94,6 +114,7 @@ class ProjectViewsTests(TestCase):
         form_fields = {
             'group': forms.fields.ChoiceField,
             'text': forms.fields.CharField,
+            'image': forms.fields.ImageField,
         }
 
         for value, expected in form_fields.items():
@@ -114,6 +135,7 @@ class ProjectViewsTests(TestCase):
         post_author_0 = author.first_name
         self.assertEqual(post_author_0, 'Тестов')
         self.assertEqual(post_text_0, 'Какой-то там текст')
+
 
     def test_home_page_show_correct_context(self):
         """Пост отображается на главной странице"""
